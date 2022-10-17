0c38c3a50fadf2293072fa0358780c78d31852e5
Sheikh sadi <you@example.com>
2022-10-18 01:53:51 +0600

    usenavigate after login added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,23 +1,25 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../contexts/UserContexts';
 
 const Login = () => {
 
      const {signIn} = useContext(AuthContext);
+     const navigate = useNavigate();
 
      const handleSubmit = event =>{
           event.preventDefault();
           const form = event.target;
           const email = form.email.value;
           const password = form.password.value;
-          console.log(email, password);
+          // console.log(email, password);
 
           signIn(email, password)
           .then(result =>{
                const user = result.user;
                console.log(user);
                form.reset();
+               navigate('/');
           })
           .catch(error => console.error(error))
      }
